764344e936531ebf8687f052f4cee8cc765cd5f5
Bohdan Matsko <bm01271970@gmail.com>
2024-04-12 22:51:37 +0200

    not_work

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,42 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+export async function searchImages(searchWord, myGallery) {
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = '43249627-6464a1b02c37cf8fbedd51288';
+
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: "dog+cat+tiger+lion",
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+    });
+try {
+        const response = await fetch(`${BASE_URL}?${params}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch images');
+        }
+        return response.json();
+    } catch (error) {
+        throw new Error('Failed to fetch images');
+    }
+}
+
+
+// fetch(`${BASE_URL}?${params}`)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error(response.status)
+//         }
+//         console.log("work");
+//         return response.json()
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.log("catch", error))
+    
+// }
---
+++ src/main.js
@@ -0,0 +1,65 @@
+import { searchImages } from './js/pixabay-api.js';
+import { renderGallery } from './js/render-functions.js';
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const form = document.querySelector('.main_form');
+const myGallery = document.querySelector('.list_gallery');
+
+const loadEl = document.querySelector('.loader');
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        loadEl.remove();
+    }, 600)
+});
+
+form.addEventListener("submit", handlerForm);
+
+async function handlerForm(event) {
+    event.preventDefault();
+    myGallery.innerHTML = '';
+    const searchWord = event.currentTarget.elements.inputElement.value;
+
+searchImages(searchWord)
+    .then(data => {
+    if (data.total == 0) {
+        iziToast.show({
+        title: 'Ops.',
+        titleColor: 'white',
+        message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        messageColor: 'white',
+        color: 'red',
+        position: 'topCenter',
+        timeout: '5000',
+        });
+        return 0;
+    } else {
+        myGallery.insertAdjacentHTML('beforeend', renderGallery(data));
+        book.refresh();
+        event.currentTarget.elements.inputElement.value = '';
+        
+    }
+    })
+    .catch(error => {
+    iziToast.show({
+        title: 'Ops.',
+        titleColor: 'white',
+        message: error,
+        messageColor: 'white',
+        color: 'red',
+        position: 'topCenter',
+        timeout: '5000',
+    });
+    })
+}
+
+
+
+
+
